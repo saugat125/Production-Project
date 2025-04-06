3996b74672d7edecaf4086cb35c59c0fbdacf436
Saugat-Rajbhandari <107553702+Saugat-Rajbhandari@users.noreply.github.com>
2025-04-06 19:10:33 +0545

    custom user model interface in admin panel

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,5 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
 # Register your models here.
-admin.site.register(User)
+
+class UserModelAdmin(BaseUserAdmin):
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserModelAdmin
+    # that reference specific fields on auth.User.
+    list_display = ["id","email", "name", "gender", "is_admin"]
+    list_filter = ["is_admin"]
+    fieldsets = [
+        ("User Credentials", {"fields": ["email", "password"]}),
+        ("Personal info", {"fields": ["name"]}),
+        ("Permissions", {"fields": ["is_admin"]}),
+    ]
+    # add_fieldsets is not a standard ModelAdmin attribute. UserModelAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "name", "gender","password1", "password2"],
+            },
+        ),
+    ]
+    search_fields = ["email"]
+    ordering = ["email","id"]
+    filter_horizontal = []
+
+admin.site.register(User, UserModelAdmin)
