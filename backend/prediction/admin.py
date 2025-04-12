a4af76dddb98cd7881a479f4cde51102b28dd106
Saugat-Rajbhandari <107553702+Saugat-Rajbhandari@users.noreply.github.com>
2025-04-12 17:03:37 +0545

    disease and doctor model added

--- backend/prediction/admin.py
+++ backend/prediction/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Disease, Doctor
 
-# Register your models here.
+class DiseaseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_specialization_display')
+    
+    def get_specialization_display(self, obj):
+        return obj.get_specialization_display()
+    get_specialization_display.short_description = 'Specialization'
+    
+admin.site.register(Disease, DiseaseAdmin)
+
+admin.site.register(Doctor)
--- backend/prediction/models.py
+++ backend/prediction/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Disease(models.Model):
+    name = models.CharField(max_length=100)
+    specialization = models.CharField(max_length=50, choices= [
+        ('dermatology', 'Dermatology'),
+        ('allergy and immunology', 'Allergy and Immunology'),
+        ('gastroenterology', 'Gastroenterology'),
+        ('infectious diseases', 'Infectious Diseases'),
+        ('endocrinology', 'Endocrinology'),
+        ('cardiology', 'Cardiology'),
+        ('pulmonology', 'Pulmonology'),
+        ('neurology', 'Neurology'),
+        ('rheumatology', 'Rheumatology'),
+        ('urology', 'Urology'),
+        ('hepatology', 'Hepatology'),
+        ('proctology', 'Proctology'),
+    ])
+
+    def __str__(self):
+        return f"{self.name}"
+    
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    specialization = models.CharField(max_length=50, choices=Disease._meta.get_field('specialization').choices)
+
+    def __str__(self):
+        return f"{self.name} - {self.specialization}"
